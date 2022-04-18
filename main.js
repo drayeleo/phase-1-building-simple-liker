76aad5e2a2f11fb27926c8b3c925c24d5f8e71fe
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-04-18 11:09:16 -0600

    commit tree b0bdb692fd49334ffa13d15564190474b42b414c

--- main.js
+++ main.js
@@ -3,8 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
+hearts.forEach(heart => {
+  heart.addEventListener('click', (event) => likeHandler(event))
+});
 
-
+function likeHandler(event) {
+  // console.log("Clicked");
+  // console.log(event.target.textContent);
+  //console.log(mimicServerCall());
+  if (event.target.textContent === EMPTY_HEART) {
+    mimicServerCall()
+    .then( () => {
+      console.log("Contacted Server");
+      event.target.textContent = FULL_HEART;
+      event.target.classList.add('activated-heart');
+    })
+    .catch( () => {
+      console.log("Error");
+      const errorMessage = document.querySelector('#modal');
+      //console.log(errorMessage);
+      errorMessage.classList.remove('hidden');
+      setTimeout( () => errorMessage.classList.add('hidden'), 3000);
+    })
+  } else {
+    event.target.textContent = EMPTY_HEART;
+    event.target.classList.remove('activated-heart');
+  }
+  
+  
+  
+  //event.target.textContent === EMPTY_HEART ? event.target.textContent = FULL_HEART : event.target.textContent = EMPTY_HEART;
+}
 
 
 //------------------------------------------------------------------------------
